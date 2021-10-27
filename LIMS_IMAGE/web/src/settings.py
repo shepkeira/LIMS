7bcaaa713dfe637c9cdbb079538ccc4a4ad7a3ee
Shaya Selincourt <sselincourt@gmail.com>
2021-10-26 22:04:27 -0700

    Stopped tracking .env and obfuscated the Django secret key

--- LIMS_IMAGE/web/src/settings.py
+++ LIMS_IMAGE/web/src/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xgps5(ey($=9*zae_*mu_zx@hflg(=2fbnd%xta2*+jjo0s(!8'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
