c48655b7ac5f879b7d915dd101237e80afe30280
shepkeira <shepherdkeira@gmail.com>
2021-11-13 14:39:23 -0800

    Customer Home Page, Employee Home Page

---
+++ LIMS_IMAGE/web/orders/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from orders import views
+
+app_name = "orders"
+
+urlpatterns = [
+    path('order_history/', views.order_history, name='order_history'),
+    path('results/', views.results, name='results'),
+    path('shopping/', views.shopping, name='shopping'),
+    path('home_page/', views.home_page, name='home'),
+]
---
+++ LIMS_IMAGE/web/training/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from training import views
+
+app_name = "training"
+
+urlpatterns = [
+    
+]
