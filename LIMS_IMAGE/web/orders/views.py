0c578e471dfd416bf8c63acf1b5bfe6800c67415
Rock-it-science <wilmc17@gmail.com>
2021-11-21 21:53:07 -0800

    Basic order page and view

--- LIMS_IMAGE/web/orders/views.py
+++ LIMS_IMAGE/web/orders/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
+from orders.models import *
 
 # Create your views here.
 def home_page(request):
     return render(request, 'orders/home_page.html')
 
 def order_history(request):
-    return render(request, 'orders/order_history.html')
+    # Get order history from model
+    order_list = Order.objects.all()
+    context_dict = {'orders': order_list}
+    return render(request, 'orders/order_history.html', context_dict)
 
 def results(request):
     return render(request, 'orders/results.html')
