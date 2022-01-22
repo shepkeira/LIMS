2d612dfccaa7b5f4468644c843ae1671ca33df36
Shaya Selincourt <sselincourt@gmail.com>
2022-01-21 18:35:19 -0800

    Improved user_tags tests with recipies and addition assertations

--- LIMS_IMAGE/web/tests/test_accounts_user_tags.py
+++ LIMS_IMAGE/web/tests/test_accounts_user_tags.py
@@ -1,36 +1,48 @@
 # Django
+from unicodedata import name
 from django.test import TestCase
 from django.contrib.auth.models import User
 from django.contrib.auth.models import Group
 
+# Third-party libraries
+from model_bakery import baker
+from model_bakery.recipe import Recipe
+
 
 class accountsUserTagsTestCase(TestCase):
     def setUp(self):
-        self.client_group = Group.objects.create(name='Client')
-        self.emp_group = Group.objects.create(name='Employee')
-        self.admin_group = Group.objects.create(name='Admin')
-        self.test_user_client = User.objects.create_user(
-            username='testuser_c', password='asdf')
-        self.test_user_client.groups.add(Group.objects.get(name='Client'))
-        self.test_user_emp = User.objects.create_user(
-            username='testuser_e', password='asdf')
-        self.test_user_emp.groups.add(Group.objects.get(name='Employee'))
-        self.test_user_admin = User.objects.create_user(
-            username='testuser_a', password='asdf')
-        self.test_user_admin.groups.add(Group.objects.get(name='Admin'))
-        self.test_user_anon = User.objects.create_user(
-            username='testuser', password='asdf')
+        # Create test groups and users (client, employee, admin, and anonymous)
+        self.client_group = baker.make_recipe(
+            'accounts.group_recipe', name='Client')
+        self.employee_group = baker.make_recipe(
+            'accounts.group_recipe', name='Employee')
+        self.admin_group = baker.make_recipe(
+            'accounts.group_recipe', name='Admin')
+        self.test_user_client = baker.make_recipe(
+            'accounts.user_recipe', groups=[self.client_group])
+        self.test_user_emp = baker.make_recipe(
+            'accounts.user_recipe', groups=[self.employee_group])
+        self.test_user_admin = baker.make_recipe(
+            'accounts.user_recipe', groups=[self.admin_group])
+        self.test_user_anon = baker.make_recipe('accounts.user_recipe')
 
     def test_has_group(self):
-        self.assertEqual(self.test_user_client.groups.filter(
-            name='Client').exists(), True)
-        self.assertEqual(self.test_user_emp.groups.filter(
-            name='Employee').exists(), True)
-        self.assertEqual(self.test_user_admin.groups.filter(
-            name='Admin').exists(), True)
-        self.assertEqual(self.test_user_anon.groups.filter(
-            name='Client').exists(), False)
-        self.assertEqual(self.test_user_anon.groups.filter(
-            name='Employee').exists(), False)
-        self.assertEqual(self.test_user_anon.groups.filter(
-            name='Admin').exists(), False)
+        # Asserts that user's group is of type Group
+        self.assertIsInstance(self.test_user_client.groups.all()[0], Group)
+        self.assertIsInstance(self.test_user_emp.groups.all()[0], Group)
+        self.assertIsInstance(self.test_user_admin.groups.all()[0], Group)
+        # Asserts if selected user has selected group
+        self.assertTrue(self.test_user_client.groups.filter(
+            name='Client').exists())
+        self.assertTrue(self.test_user_emp.groups.filter(
+            name='Employee').exists())
+        self.assertTrue(self.test_user_admin.groups.filter(
+            name='Admin').exists())
+
+        # Asserts that anonymous user is not in any selected group
+        self.assertFalse(self.test_user_anon.groups.filter(
+            name='Client').exists())
+        self.assertFalse(self.test_user_anon.groups.filter(
+            name='Employee').exists())
+        self.assertFalse(self.test_user_anon.groups.filter(
+            name='Admin').exists())
