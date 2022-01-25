c58d1b15bb14356c557ca13654d54e566955c574
shepkeira <shepherdkeira@gmail.com>
2022-01-24 20:06:02 -0800

    Redirect non-authenticated users

--- LIMS_IMAGE/web/laboratory/views.py
+++ LIMS_IMAGE/web/laboratory/views.py
@@ -3,6 +3,8 @@ from accounts.models import Client
 
 # Create your views here.
 def home_page(request):
-    if Client.objects.filter(user=request.user):
-        return redirect("accounts:customer_home_page")
-    return render(request, 'laboratory/home_page.html')
+    if request.user.is_authenticated:
+        if Client.objects.filter(user=request.user):
+            return redirect("accounts:customer_home_page")
+        return render(request, 'laboratory/home_page.html')
+    return redirect("/")
