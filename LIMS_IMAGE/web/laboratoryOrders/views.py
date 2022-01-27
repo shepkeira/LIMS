1aecee30009f490d8696d01c79fbcf0e8ebd99e4
SE34 <everdens@student.ubc.ca>
2022-01-27 11:19:49 -0800

    commiting to change branch still

--- LIMS_IMAGE/web/laboratoryOrders/views.py
+++ LIMS_IMAGE/web/laboratoryOrders/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import *
+#Create your views here.
+# def sample_types(request):
+#     sample_types = Sample.objects.all()
+#     return render(request,'shopping/shopping.html',{'sample_types':sample_types})
