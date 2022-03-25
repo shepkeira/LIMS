41ed50ed16f339f17bd9adc40ba87ad515f6ed47
Rock-it-science <wilmc17@gmail.com>
2022-03-25 00:46:43 -0700

    finished most lab views tests

--- LIMS_IMAGE/web/tests/test_laboratory_views.py
+++ LIMS_IMAGE/web/tests/test_laboratory_views.py
@@ -23,6 +23,9 @@ class laboratoryViewsTestCase(TestCase):
             username='testuser_c', password='asdf')
         self.test_user_emp = User.objects.create_user(
             username='testuser_e', password='asdf')
+        self.test_user_admin = User.objects.create_user(
+            username='testuser_a', password='asdf'
+        )
 
         self.test_client = baker.make_recipe(
             'accounts.client_recipe',
@@ -34,8 +37,16 @@ class laboratoryViewsTestCase(TestCase):
             user=self.test_user_emp
         )
 
+        self.test_labadmin = baker.make_recipe(
+            'accounts.labadmin_recipe',
+            user=self.test_user_admin
+        )
+
         self.sample = baker.make(Sample)
-        self.order_sample = baker.make(OrderSample, sample = self.sample)
+        self.order = baker.make(Order)
+        self.order_sample = baker.make(OrderSample, order = self.order, sample = self.sample)
+        self.lab_sample = baker.make(LabSample, sample = self.sample)
+        self.test_sample = baker.make(TestSample, lab_sample_id = self.lab_sample)
 
     def test_lab_home_page_unauth(self):
         # Unauthenticated user
@@ -127,6 +138,21 @@ class laboratoryViewsTestCase(TestCase):
         self.client.login(username='testuser_c', password='asdf')
         response = self.client.get('/laboratory/distribute_sample/' + str(self.sample.id))
         self.assertEqual(response.status_code, 302)
+
+    def test_create_test_sample(self):
+        # Authenticated user
+        self.client.login(username='testuser_e', password='asdf')
+        response = self.client.get('/laboratory/analysis_sample/' + str(self.lab_sample.id))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'laboratory/analysis_sample.html')
+
+        # Unauthenticaed user or authenticated client
+        self.client.logout()
+        response = self.client.get('/laboratory/analysis_sample/' + str(self.lab_sample.id))
+        self.assertRedirects(response, '/')
+        self.client.login(username='testuser_c', password='asdf')
+        response = self.client.get('/laboratory/analysis_sample/' + str(self.lab_sample.id))
+        self.assertEqual(response.status_code, 302)
     
     def test_order_list(self):
         # Authenticated user
@@ -141,4 +167,164 @@ class laboratoryViewsTestCase(TestCase):
         self.assertRedirects(response, '/')
         self.client.login(username='testuser_c', password='asdf')
         response = self.client.get('/laboratory/order_list/')
+        self.assertEqual(response.status_code, 302)
+
+
+    def test_create_test(self):
+        # Authenticated admin
+        self.client.login(username='testuser_a', password='asdf')
+        response = self.client.get('/laboratory/create_test/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'laboratory/test_create.html')
+
+        # Unauthenticaed user or authenticated client
+        self.client.logout()
+        response = self.client.get('/laboratory/create_test/')
+        self.assertRedirects(response, '/')
+        self.client.login(username='testuser_c', password='asdf')
+        response = self.client.get('/laboratory/create_test/')
+        self.assertEqual(response.status_code, 302)
+
+
+    def test_create_location(self):
+        # Authenticated admin
+        self.client.login(username='testuser_a', password='asdf')
+        response = self.client.get('/laboratory/create_location/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'laboratory/lab_create.html')
+
+        # Unauthenticaed user or authenticated client
+        self.client.logout()
+        response = self.client.get('/laboratory/create_location/')
+        self.assertRedirects(response, '/')
+        self.client.login(username='testuser_c', password='asdf')
+        response = self.client.get('/laboratory/create_location/')
+        self.assertEqual(response.status_code, 302)
+
+
+    def test_admin_page(self):
+        # Authenticated admin
+        self.client.login(username='testuser_a', password='asdf')
+        response = self.client.get('/laboratory/admin_home_page/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'laboratory/admin_home_page.html')
+
+        # Unauthenticaed user or authenticated client
+        self.client.logout()
+        response = self.client.get('/laboratory/admin_home_page/')
+        self.assertRedirects(response, '/')
+        self.client.login(username='testuser_c', password='asdf')
+        response = self.client.get('/laboratory/admin_home_page/')
+        self.assertEqual(response.status_code, 302)
+
+
+    def test_validate_sample(self):
+        # Authenticated employee
+        self.client.login(username='testuser_e', password='asdf')
+        response = self.client.get('/laboratory/validate_sample/'+str(self.sample.id)+'/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'laboratory/validate.html')
+
+        # Unauthenticaed user or authenticated client
+        self.client.logout()
+        response = self.client.get('/laboratory/validate_sample/'+str(self.sample.id)+'/')
+        self.assertRedirects(response, '/')
+        self.client.login(username='testuser_c', password='asdf')
+        response = self.client.get('/laboratory/validate_sample/'+str(self.sample.id)+'/')
+        self.assertEqual(response.status_code, 302)
+
+
+    def test_view_order(self):
+        # Authenticated employee
+        self.client.login(username='testuser_e', password='asdf')
+        response = self.client.get('/laboratory/order/'+str(self.order.id))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'laboratory/view_order.html')
+
+        # Unauthenticaed user or authenticated client
+        self.client.logout()
+        response = self.client.get('/laboratory/order/'+str(self.order.id))
+        self.assertRedirects(response, '/')
+        self.client.login(username='testuser_c', password='asdf')
+        response = self.client.get('/laboratory/order/'+str(self.order.id))
+        self.assertEqual(response.status_code, 302)
+
+
+    def test_view_sample(self):
+        # Authenticated employee
+        self.client.login(username='testuser_e', password='asdf')
+        response = self.client.get('/laboratory/sample/'+str(self.sample.id))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'laboratory/view_sample.html')
+
+        # Unauthenticaed user or authenticated client
+        self.client.logout()
+        response = self.client.get('/laboratory/sample/'+str(self.sample.id))
+        self.assertRedirects(response, '/')
+        self.client.login(username='testuser_c', password='asdf')
+        response = self.client.get('/laboratory/sample/'+str(self.sample.id))
+        self.assertEqual(response.status_code, 302)
+
+
+    def test_view_lab_sample(self):
+        # Authenticated employee
+        self.client.login(username='testuser_e', password='asdf')
+        response = self.client.get('/laboratory/lab_sample/'+str(self.lab_sample.id))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'laboratory/view_lab_sample.html')
+
+        # Unauthenticaed user or authenticated client
+        self.client.logout()
+        response = self.client.get('/laboratory/lab_sample/'+str(self.lab_sample.id))
+        self.assertRedirects(response, '/')
+        self.client.login(username='testuser_c', password='asdf')
+        response = self.client.get('/laboratory/lab_sample/'+str(self.lab_sample.id))
+        self.assertEqual(response.status_code, 302)
+
+
+    def test_view_test_sample(self):
+        # Authenticated employee
+        self.client.login(username='testuser_e', password='asdf')
+        response = self.client.get('/laboratory/test_sample/'+str(self.test_sample.id))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'laboratory/view_test_sample.html')
+
+        # Unauthenticaed user or authenticated client
+        self.client.logout()
+        response = self.client.get('/laboratory/test_sample/'+str(self.test_sample.id))
+        self.assertRedirects(response, '/')
+        self.client.login(username='testuser_c', password='asdf')
+        response = self.client.get('/laboratory/test_sample/'+str(self.test_sample.id))
+        self.assertEqual(response.status_code, 302)
+
+
+    def test_inventory(self):
+        # Authenticated employee
+        self.client.login(username='testuser_e', password='asdf')
+        response = self.client.get('/laboratory/inventory/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'laboratory/inventory.html')
+
+        # Unauthenticaed user or authenticated client
+        self.client.logout()
+        response = self.client.get('/laboratory/inventory/')
+        self.assertRedirects(response, '/')
+        self.client.login(username='testuser_c', password='asdf')
+        response = self.client.get('/laboratory/inventory/')
+        self.assertEqual(response.status_code, 302)
+
+
+    def test_lab_analysis(self):
+        # Authenticated employee
+        self.client.login(username='testuser_e', password='asdf')
+        response = self.client.get('/laboratory/analyisis/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'laboratory/inventory.html')
+
+        # Unauthenticaed user or authenticated client
+        self.client.logout()
+        response = self.client.get('/laboratory/inventory/')
+        self.assertRedirects(response, '/')
+        self.client.login(username='testuser_c', password='asdf')
+        response = self.client.get('/laboratory/inventory/')
         self.assertEqual(response.status_code, 302)
