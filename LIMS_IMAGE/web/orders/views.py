1219665965fa21ae7487ab26f2f385f85ea40a43
shepkeira <shepherdkeira@gmail.com>
2022-03-31 19:27:46 -0700

    Fixing results page

--- LIMS_IMAGE/web/orders/views.py
+++ LIMS_IMAGE/web/orders/views.py
@@ -37,27 +37,36 @@ def results(request):
         return redirect("accounts:employee_home_page")
     context = RequestContext(request)
     # get a list of test ids realted to this user
-    test_ids = OrderTest.test_ids_for_user(request.user)
+    order_test_ids = OrderTest.test_ids_for_user(request.user)
     results_list = {}
     # use the test ids to get the results for the tests
-    for order_number, test_ids in test_ids.items():
+    for order_number, test_ids in order_test_ids.items():
         results_list[order_number] = TestResult.get_test_results(test_ids)
-    
     render_results = {}
 
     # re order the results for display purposes: render_results[order_number] = render_results[order_number] + list(query_set)
     for order_number, list_of_query_sets in results_list.items():
         render_results[order_number] = []
-        for query_set in list_of_query_sets:
-            list_query_set = list(query_set)
+        if len(list_of_query_sets) >= 1:
             sample_dict = {}
-            for testresult in list_query_set:
-                result_dict = {}
-                result_dict['status'] = testresult.status
-                result_dict['result'] = testresult.result
-                result_dict['test'] = testresult.test_id.user_side_id()
-                result_dict['order_number'] = order_number
-                sample_dict[result_dict['test']] = result_dict
+            for query_set in list_of_query_sets:
+                list_query_set = list(query_set)
+                for testresult in list_query_set:
+                    result_dict = {}
+                    result_dict['status'] = testresult.status
+                    result_dict['result'] = testresult.result
+                    result_dict['test'] = testresult.test_id.user_side_id()
+                    result_dict['order_number'] = order_number
+                    sample_dict[result_dict['test']] = result_dict
+                render_results[order_number] = sample_dict
+        else:
+            sample_dict = {}
+            result_dict = {}
+            result_dict['status'] = "not recieved"
+            result_dict['result'] = "--"
+            result_dict['test'] = "--"
+            result_dict['order_number'] = order_number
+            sample_dict[result_dict['test']] = result_dict
             render_results[order_number] = sample_dict
             
     context_dict = {'user': request.user, 'results': render_results}
