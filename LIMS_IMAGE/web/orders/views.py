bc4800cb17a1af73b65a69f09f6f45fcd533c3dd
shepkeira <shepherdkeira@gmail.com>
2022-04-01 08:38:33 -0700

    Fix order results

--- LIMS_IMAGE/web/orders/views.py
+++ LIMS_IMAGE/web/orders/views.py
@@ -37,27 +37,29 @@ def results(request):
         return redirect("accounts:employee_home_page")
     context = RequestContext(request)
     # get a list of test ids realted to this user
-    order_test_ids = OrderTest.test_ids_for_user(request.user)
-    results_list = {}
-    # use the test ids to get the results for the tests
-    for order_number, test_ids in order_test_ids.items():
-        results_list[order_number] = TestResult.get_test_results(test_ids)
-    render_results = {}
+    orders = Order.order_for_user(request.user)
+    order_test_samples = {}
+    for order in orders:
+        test_samples = []
+        order_samples = OrderSample.objects.filter(order=order)
+        for order_sample in order_samples:
+            test_samples += list(order_sample.sample.test_samples())
+        order_test_samples[order] = test_samples
 
+    render_results = {}
     # re order the results for display purposes: render_results[order_number] = render_results[order_number] + list(query_set)
-    for order_number, list_of_query_sets in results_list.items():
+    for order_number, test_samples in order_test_samples.items():
         render_results[order_number] = []
-        if len(list_of_query_sets) >= 1:
+        if len(test_samples) >= 1:
             sample_dict = {}
-            for query_set in list_of_query_sets:
-                list_query_set = list(query_set)
-                for testresult in list_query_set:
-                    result_dict = {}
-                    result_dict['status'] = testresult.status
-                    result_dict['result'] = testresult.result
-                    result_dict['test'] = testresult.test_id.user_side_id()
-                    result_dict['order_number'] = order_number
-                    sample_dict[result_dict['test']] = result_dict
+            for test_sample in test_samples:
+                testresult = test_sample.test_result()
+                result_dict = {}
+                result_dict['status'] = testresult.status
+                result_dict['result'] = testresult.result
+                result_dict['test'] = test_sample.user_side_id()
+                result_dict['order_number'] = order_number.order_number
+                sample_dict[result_dict['test']] = result_dict
                 render_results[order_number] = sample_dict
         else:
             sample_dict = {}
@@ -65,7 +67,7 @@ def results(request):
             result_dict['status'] = "not recieved"
             result_dict['result'] = "--"
             result_dict['test'] = "--"
-            result_dict['order_number'] = order_number
+            result_dict['order_number'] = order_number.order_number
             sample_dict[result_dict['test']] = result_dict
             render_results[order_number] = sample_dict
             
